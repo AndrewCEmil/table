5bb8eb29ee2ecb726de4adbd385998893f7d52cb
ace <andrew.emil@gmail.com>
2017-07-20 16:18:49 -0400

    Added a bunch of random stuff to table, including deformer

---
+++ Assets/Scripts/Deformer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deformer : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		MeshFilter meshFilter = gameObject.GetComponent<MeshFilter> ();
+		Mesh mesh = meshFilter.mesh;
+		Vector3[] baseVerticies = mesh.vertices;
+		Vector3[] vertices = new Vector3[baseVerticies.Length];
+
+		float timez = Time.time;
+		for (var i=0; i < baseVerticies.Length; i++) {
+			var vertex = baseVerticies[i];
+			float noise = Mathf.PerlinNoise(timez + vertex.x, timez + vertex.y);
+			vertex.z = noise;
+			vertices[i] = vertex;
+		}
+
+		mesh.vertices = vertices;
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+	}
+}
