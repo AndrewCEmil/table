42349acea3fa2a4d73aaff5d170218b63f487605
ace <andrew.emil@gmail.com>
2017-08-27 15:57:42 -0400

    Added chains, trails, line

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour {
+
+	// Use this for initialization
+	private bool pushed;
+	private Rigidbody rb;
+	void Start () {
+		pushed = false;
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!pushed) {
+			Push ();
+			pushed = true;
+		}
+	}
+
+	void Push() {
+		rb.AddForce (new Vector3 (0, 0, 2000), ForceMode.Impulse);
+	}
+}
