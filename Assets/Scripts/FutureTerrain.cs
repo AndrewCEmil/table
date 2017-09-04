94f142b2854d3902e1e61246031d728539afe613
ace <andrew.emil@gmail.com>
2017-09-04 15:32:32 -0400

    Finally generating mesh lol

--- Assets/Scripts/FutureTerrain.cs
+++ Assets/Scripts/FutureTerrain.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class FutureTerrain : MonoBehaviour {
 
-	public int xSize;
-	public int ySize;
+	private int width;
 	private Vector3[] verticies;
 	List<GameObject> terrainMarkers;
 	// Use this for initialization
 	void Start () {
+		width = 10;
+
+		InitTerrainMarkers ();
+		GenerateGrid ();
+		//Crumple ();
+	}
+
+	private void InitTerrainMarkers() {
 		GameObject[] terrainMarkersArray = GameObject.FindGameObjectsWithTag ("TerrainMarker");
 		terrainMarkers = new List<GameObject> (terrainMarkersArray);
-
-		GenerateGrid ();
-		Crumple ();
+		terrainMarkers.Sort(delegate(GameObject x, GameObject y)
+			{
+				return x.name.CompareTo(y.name);
+			});
 	}
 
 	private void Crumple() {
@@ -38,38 +46,68 @@ public class FutureTerrain : MonoBehaviour {
 	}
 
 	private void GenerateGrid() {
+		for (int i = 1; i < terrainMarkers.Count; i++) {
+			GenerateGridForPair (terrainMarkers [i - 1].transform.position, terrainMarkers [i].transform.position, i);
+		}
+	}
+
+	//TODO make sure y vs z is right here
+	private void GenerateGridForPair(Vector3 first, Vector3 second, int gridNum) {
 		Mesh mesh = new Mesh ();
-		mesh.name = "FutureTerrain";
+		mesh.name = "FutureTerrain" + gridNum;
 		GetComponent<MeshFilter> ().mesh = mesh;
-		verticies = new Vector3[(xSize + 1) * (ySize + 1)];
+
+		Vector3 yAxis = new Vector3 (0, (second.y + first.y) / 2f + 1, 0).normalized;
+		Vector3 lineAxis = (second - first).normalized;
+		Vector3 widthAxis = Vector3.Cross (lineAxis, yAxis).normalized;
+		Vector3 topLeft = second + (widthAxis * width / 2f);
+		Vector3 bottomRight = first - (widthAxis * width / 2f);
+		Vector3 origin = bottomRight;
+		int length = (int) Mathf.Floor (Vector3.Distance (first, second));
+
+		verticies = new Vector3[(width + 1) * (length + 1)];
 		Vector2[] uv = new Vector2[verticies.Length];
-		for (int i = 0, y = 0; y <= ySize; y++) {
-			for (int x = 0; x <= xSize; x++, i++) {
-				verticies [i] = new Vector3 (x, y);
-				uv [i] = new Vector2 ((float)x / xSize, (float)y / ySize);
+		for (int i = 0, y = 0; y <= length; y++) {
+			for (int x = 0; x <= width; x++, i++) {
+				verticies [i] = GetPoint (x, y, lineAxis, widthAxis, origin);
+				uv [i] = new Vector2 ((float)x / width, (float)y / length);
 			}
 		}
 
 		mesh.vertices = verticies;
 		mesh.uv = uv;
 
-		int[] triangles = new int[xSize * ySize * 6];
-		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
-			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+		int[] triangles = new int[width * length * 6];
+		for (int ti = 0, vi = 0, y = 0; y < length; y++, vi++) {
+			for (int x = 0; x < width; x++, ti += 6, vi++) {
 				triangles[ti] = vi;
 				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-				triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
-				triangles[ti + 5] = vi + xSize + 2;
+				triangles[ti + 4] = triangles[ti + 1] = vi + width + 1;
+				triangles[ti + 5] = vi + width + 2;
 			}
 		}
+
+
 		mesh.triangles = triangles;
 
 		mesh.RecalculateNormals();
+		Vector3[] normals = mesh.normals;
+		for(int i = 0; i < normals.Length; i++) {
+			normals [i] = yAxis;
+		}
+		mesh.normals = normals;
 		mesh.MarkDynamic ();
+	}
+
+
+	private Vector3 GetPoint(int x, int y, Vector3 lineAxis, Vector3 widthAxis, Vector3 origin) {
+		return (lineAxis * y) + (widthAxis * x) + origin;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		Vector3[] normals = mesh.normals;
+		Debug.Log ("test");
 	}
 }
