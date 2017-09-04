81a8926fdfb0e39cb09665b57e21db46977cc087
ace <andrew.emil@gmail.com>
2017-09-04 15:35:33 -0400

    Holy smokes finally got it all right

--- Assets/Scripts/FutureTerrain.cs
+++ Assets/Scripts/FutureTerrain.cs
@@ -81,8 +81,8 @@ public class FutureTerrain : MonoBehaviour {
 		for (int ti = 0, vi = 0, y = 0; y < length; y++, vi++) {
 			for (int x = 0; x < width; x++, ti += 6, vi++) {
 				triangles[ti] = vi;
-				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-				triangles[ti + 4] = triangles[ti + 1] = vi + width + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + 1;
+				triangles[ti + 3] = triangles[ti + 2] = vi + width + 1;
 				triangles[ti + 5] = vi + width + 2;
 			}
 		}
