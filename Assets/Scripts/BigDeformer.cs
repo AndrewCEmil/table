a89dc6b64011261d1e7e564eab33d362762c5d85
ace <andrew.emil@gmail.com>
2017-09-08 12:27:14 -0400

    Initial work for cool antechamber

---
+++ Assets/Scripts/BigDeformer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigDeformer : MonoBehaviour {
+
+	public float maxDistance;
+	public float maxHeight;
+	private Vector3 middlePosition;
+	private Vector3 storedVector;
+	bool deformed;
+	int count;
+	void Start() {
+		deformed = false;
+		count = 0;
+		middlePosition = new Vector3 (500, 0, 500);
+		storedVector = new Vector3 (0, 0, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(!deformed) {
+			MeshFilter meshFilter = gameObject.GetComponent<MeshFilter> ();
+			Mesh mesh = meshFilter.mesh;
+			Vector3[] baseVerticies = mesh.vertices;
+			Vector3[] vertices = new Vector3[baseVerticies.Length];
+
+			float timez = Time.time + 3f;
+			for (var i=0; i < baseVerticies.Length; i++) {
+				Vector3 vertex = baseVerticies[i];
+				float scale = GetScale (vertex);
+				float noise = Mathf.PerlinNoise (timez + vertex.x, timez + vertex.z);
+				vertex.y = noise * scale;
+				vertices[i] = vertex;
+			}
+
+			mesh.vertices = vertices;
+			mesh.RecalculateNormals();
+			mesh.RecalculateBounds();
+		}
+	}
+
+	float GetScale(Vector3 position) {
+		return maxHeight * (GetRadius(position) / maxDistance);
+	}
+
+	float GetRadius(Vector3 position) {
+		storedVector.x = position.x;
+		storedVector.z = position.z;
+		return Vector3.Distance (storedVector, middlePosition);
+	}
+
+}
---
+++ Assets/Scripts/BigGrid.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class BigGrid : MonoBehaviour {
+	public int xSize;
+	public int ySize;
+	public int scale;
+	private Vector3[] verticies;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void Awake () {
+		Generate();
+	}
+
+	private void Generate() {
+		Mesh mesh = new Mesh ();
+		mesh.name = "MovingMesh";
+		GetComponent<MeshFilter> ().mesh = mesh;
+		verticies = new Vector3[(xSize + 1) * (ySize + 1)];
+		Vector2[] uv = new Vector2[verticies.Length];
+		for (int i = 0, y = 0; y <= ySize * scale; y += scale) {
+			for (int x = 0; x <= xSize * scale; x += scale, i++) {
+				verticies [i] = new Vector3 (x, 0, y);
+				uv [i] = new Vector2 ((float)x / xSize * scale, (float)y / ySize * scale);
+			}
+		}
+
+		mesh.vertices = verticies;
+		mesh.uv = uv;
+
+		int[] triangles = new int[xSize * ySize * 6];
+		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+				triangles[ti + 5] = vi + xSize + 2;
+			}
+		}
+		mesh.triangles = triangles;
+
+		mesh.RecalculateNormals();
+		mesh.MarkDynamic ();
+	}
+}
