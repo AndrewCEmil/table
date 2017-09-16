b4f5795c5f395e5a9304736b5e3966de5d2266c2
ace <andrew.emil@gmail.com>
2017-09-16 12:27:23 -0400

    Support for canyons, it sucks

--- Assets/Scripts/CanyonCreator.cs
+++ Assets/Scripts/CanyonCreator.cs
@@ -2,39 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Lol this code is so ugly...really should fix to make it more reusable etc whatever
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class CanyonCreator : MonoBehaviour {
 
 	// Use this for initialization
 	private List<GameObject> terrainMarkers;
+	private Vector3[] verticies;
 	private float maxX;
+	private float width;
 	void Start () {
+	}
+
+	public void Init() {
 		InitVariables ();
-		InitTerrainMarkers ();
-		Generate ();
 	}
 
 	private void InitVariables() {
 		maxX = 100f;
+		width = maxX;
 	}
 
-	private void InitTerrainMarkers() {
-		GameObject[] tunnelMarkersArray = GameObject.FindGameObjectsWithTag ("TerrainMarker");
-		terrainMarkers = new List<GameObject> (tunnelMarkersArray);
-		terrainMarkers.Sort(delegate(GameObject x, GameObject y)
-			{
-				return x.name.CompareTo(y.name);
-			});
+	public void Generate(Vector3 first, Vector3 second, int gridNum) {
+		GenerateGridForPair (first, second, gridNum);
+		GenerateForPair (first, second);
 	}
 
-	private void Generate() {
-		for (int i = 0; i < terrainMarkers.Count - 1; i++) {
-			GenerateForPair (terrainMarkers [i], terrainMarkers [i + 1]);
+	private void GenerateGridForPair(Vector3 first, Vector3 second, int gridNum) {
+		Mesh mesh = new Mesh ();
+		mesh.name = "Canyon" + gridNum;
+		GetComponent<MeshFilter> ().mesh = mesh;
+
+		int length = (int) Mathf.Floor (Vector3.Distance (first, second));
+		verticies = new Vector3[((int)width + 1) * (length + 1)];
+		Vector2[] uv = new Vector2[verticies.Length];
+		for (int i = 0, y = 0; y <= length; y++) {
+			for (int x = 0; x <= width; x++, i++) {
+				verticies [i] = new Vector3 (x, 0, y);
+				uv [i] = new Vector2 ((float)x / width, (float)y / length);
+			}
 		}
+
+		mesh.vertices = verticies;
+		mesh.uv = uv;
+
+		int[] triangles = new int[(int)width * length * 6];
+		for (int ti = 0, vi = 0, y = 0; y < length; y++, vi++) {
+			for (int x = 0; x < width; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + (int)width + 1;
+				triangles[ti + 5] = vi + (int)width + 2;
+			}
+		}
+
+
+		mesh.triangles = triangles;
+
+		mesh.RecalculateNormals();
+		mesh.MarkDynamic ();
+		mesh.RecalculateBounds ();
 	}
+
 
 	//Assume that we have an grid already of the right size
-	private void GenerateForPair(GameObject a, GameObject b) {
+	private void GenerateForPair(Vector3 aPos, Vector3 bPos) {
 		MeshFilter meshFilter = gameObject.GetComponent<MeshFilter> ();
 		Mesh mesh = meshFilter.mesh;
 		Vector3[] baseVerticies = mesh.vertices;
@@ -43,7 +74,7 @@ public class CanyonCreator : MonoBehaviour {
 		Vector3 width;
 		Vector3 up;
 		float radius = 10f;
-		forward = (b.transform.position - a.transform.position).normalized;
+		forward = (bPos - aPos).normalized;
 		up = (forward + new Vector3 (0, 1, 0)).normalized;
 		width = Vector3.Cross (forward, up).normalized;
 		up = Vector3.Cross (forward, width).normalized;
---
+++ Assets/Scripts/CanyonGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanyonGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	private GameObject baseCanyon;
+	private List<GameObject> terrainMarkers;
+	private List<GameObject> canyons;
+	void Start () {
+		canyons = new List<GameObject> ();
+		baseCanyon = GameObject.Find ("BaseCanyon");
+		InitTerrainMarkers ();
+		Generate ();
+	}	
+
+	private void InitTerrainMarkers() {
+		GameObject[] tunnelMarkersArray = GameObject.FindGameObjectsWithTag ("TerrainMarker");
+		terrainMarkers = new List<GameObject> (tunnelMarkersArray);
+		terrainMarkers.Sort(delegate(GameObject x, GameObject y)
+			{
+				return x.name.CompareTo(y.name);
+			});
+	}
+
+	private void Generate() {
+		for (int i = 0; i < terrainMarkers.Count - 1; i++) {
+			GenerateGrid (terrainMarkers [i].transform.position, terrainMarkers [i + 1].transform.position, i);
+		}
+	}
+
+	private void GenerateGrid(Vector3 aPos, Vector3 bPos, int count) {
+		GameObject canyon = Instantiate (baseCanyon);
+		CanyonCreator canyonCreator = canyon.GetComponent<CanyonCreator> ();
+		canyonCreator.Init ();
+		canyonCreator.Generate (aPos, bPos, count);
+		canyons.Add (canyon);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
